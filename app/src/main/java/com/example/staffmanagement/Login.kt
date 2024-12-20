d01c8a6ef88225312302d7aab1d53e9e6a5688a9
Krishnam Mavani <krishnammavani03@gmail.com>
2024-12-20 22:37:47 +0530

    20/12/2024 (22:36) [Krishna]
    => Complete:
      - loading logo position
      - login screen complete without button click
    
    => Pending:
      - button click event

--- app/src/main/java/com/example/staffmanagement/Login.kt
+++ app/src/main/java/com/example/staffmanagement/Login.kt
@@ -1,6 +1,11 @@
 package com.example.staffmanagement
 
+import android.graphics.Color
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +14,26 @@ import androidx.core.view.WindowInsetsCompat
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val _text = findViewById<TextView>(R.id.registrTxt)
+        val fullText = "Don't have an account? Register"
+        val spannable = SpannableString(fullText)
+
+        spannable.setSpan(
+            ForegroundColorSpan(Color.parseColor("#7D7E7E")),
+            0,
+            22,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        spannable.setSpan(
+            ForegroundColorSpan(Color.parseColor("#0A2241")),
+            23,
+            fullText.length,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        _text.text = spannable
     }
 }
