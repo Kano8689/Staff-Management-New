b23f2eca09d790a52b5ed84295f1e69571171eab
Krishnam Mavani <krishnammavani03@gmail.com>
2024-12-23 23:01:40 +0530

    23/12/2024 (23:001 [Krishna]
    - Dashboard basic UI
    - Poppins font family all font ad

--- app/src/main/java/com/example/staffmanagement/DashboardMain.kt
+++ app/src/main/java/com/example/staffmanagement/DashboardMain.kt
@@ -1,5 +1,6 @@
 package com.example.staffmanagement
 
+import android.graphics.Color
 import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
 import android.widget.ImageView
@@ -10,6 +11,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class DashboardMain : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,11 +19,31 @@ class DashboardMain : AppCompatActivity() {
 
         val shape = GradientDrawable()
         shape.shape = GradientDrawable.RECTANGLE
-        shape.cornerRadius = 16f // Set corner radius (in dp or px)
-        shape.setColor(ContextCompat.getColor(this, R.color.white)) // Set color
+        shape.cornerRadius = 80f // Set corner radius (in dp or px)
+        shape.setColor(ContextCompat.getColor(this, R.color.offWhite)) // Set color
 
         val whiteBG = findViewById<ImageView>(R.id.bg_white)
         whiteBG.background = shape
+
+        val DashboardItems: List<ImageView> = listOf(
+            findViewById(R.id.opt1),
+            findViewById(R.id.opt2),
+            findViewById(R.id.opt3),
+            findViewById(R.id.opt4),
+            findViewById(R.id.opt5),
+            findViewById(R.id.opt6)
+        )
+
+        DashboardItems.forEach {
+            // Create a new GradientDrawable for each ImageView
+            val Shape = GradientDrawable().apply {
+                cornerRadius = 80f
+                setColor(ContextCompat.getColor(this@DashboardMain, R.color.white))
+            }
+
+            // Set the background for each ImageView
+            it.background = Shape
+        }
 
     }
 }
