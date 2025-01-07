1e3fb28679cafe5b803749a32b5f839838929f46
Krishnam Mavani <krishnammavani03@gmail.com>
2025-01-07 22:34:16 +0530

    07/01/2025 (22:34) [Krishna]
    - select date
    - select date print
    - select day print

--- app/src/main/java/com/example/staffmanagement/Attedence.kt
+++ app/src/main/java/com/example/staffmanagement/Attedence.kt
@@ -2,11 +2,14 @@ package com.example.staffmanagement
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.CalendarView
 import android.widget.ImageButton
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.time.LocalDate
 
 class Attedence : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,5 +22,23 @@ class Attedence : AppCompatActivity() {
             this.startActivity(Intent(this,DashboardMain::class.java))
         }
 
+        val date: TextView = findViewById(R.id.date)
+        val day: TextView = findViewById(R.id.day)
+
+        val calendarView: CalendarView = findViewById(R.id.calendarView)
+        calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
+            val dayName = getDayName(dayOfMonth, month+1, year)
+            val selectedDate = "$dayOfMonth,${getMonthName(month+1)} $year"
+            date.text = selectedDate
+            day.text = dayName
+        }
+    }
+    fun getMonthName(index: Int): MonthName? {
+        return if (index in 1..12) MonthName.values()[index - 1] else null
+    }
+    fun getDayName(day: Int, month: Int, year: Int): String {
+        val date = LocalDate.of(year, month, day)
+        val dayOfWeek = date.dayOfWeek
+        return dayOfWeek.name.lowercase().replaceFirstChar { it.uppercase() }
     }
 }
--- app/src/main/java/com/example/staffmanagement/ProjectTask.kt
+++ app/src/main/java/com/example/staffmanagement/ProjectTask.kt
@@ -3,11 +3,35 @@ package com.example.staffmanagement
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.CalendarView
 import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import java.time.LocalDate
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+
+enum class MonthName(val start: Int) {
+    January(0),
+    February(1),
+    March(2),
+    April(3),
+    May(4),
+    June(5),
+    July(6),
+    August(7),
+    September(8),
+    October(9),
+    November(10),
+    December(11);
+
+    fun getMonthNumber(month:Int): Int {
+        return start
+    }
+}
+
 
 class ProjectTask : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,5 +47,24 @@ class ProjectTask : AppCompatActivity() {
             this.startActivity(Intent(this,DashboardMain::class.java))
         }
 
+        val date: TextView = findViewById(R.id.date)
+        val day:TextView = findViewById(R.id.day)
+
+        val calendarView: CalendarView = findViewById(R.id.calendarView)
+        calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
+            val dayName = getDayName(dayOfMonth, month+1, year)
+            val selectedDate = "$dayOfMonth,${getMonthName(month+1)} $year"
+            date.text = selectedDate
+            day.text = dayName
+        }
+
+    }
+    fun getMonthName(index: Int): MonthName? {
+        return if (index in 1..12) MonthName.values()[index - 1] else null
+    }
+    fun getDayName(day: Int, month: Int, year: Int): String {
+        val date = LocalDate.of(year, month, day)
+        val dayOfWeek = date.dayOfWeek
+        return dayOfWeek.name.lowercase().replaceFirstChar { it.uppercase() }
     }
 }
